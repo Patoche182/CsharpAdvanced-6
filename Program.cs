95bbd3426bf0ecb0791b306ec3cd2decd96fc25d
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-29 00:17:44 +0100

    FIX : Start erreur ;

--- Program.cs
+++ Program.cs
@@ -3,15 +3,29 @@ using System.Linq;
 
 namespace CsharpAdvanced_6
 {
-
     //Class Client
-    internal class Client
+    class Client
     {
         public int id;
         public string nom;
         public int budget;
         public string mail;
 
+        public Client(int i, string n, int b, string m)
+        {
+            id = i;
+            nom = n;
+            budget = b;
+            mail = m;
+        }
+        public override string ToString()
+        {
+            return $"{id} + {nom} + {mail} + {budget}";
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
