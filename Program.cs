5de8e8a08a596981b111e80341aacdf185817e65
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-28 23:59:17 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace CsharpAdvanced_6
+{
+    internal class Client
+    {
+        public int id;
+        public string nom;
+        public int budget;
+        public string mail;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
