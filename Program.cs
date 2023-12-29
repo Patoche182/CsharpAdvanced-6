404f0449cef4be6a90a2fd97d46044a94a7ef84a
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-29 02:14:00 +0100

    ADD : Clients

--- Program.cs
+++ Program.cs
@@ -20,12 +20,21 @@ namespace CsharpAdvanced_6
         }
         public override string ToString()
         {
-            return $"{id} + {nom} + {mail} + {budget}";
+            return $"{id} + {nom} + {mail} + {budget}"; // Chaine Formatée
         }
     }
 
     class Program
     {
+        List<Client> clients = new List<Client>()
+        {
+            new Client(1, "Patrice", 10000, "patrice@mail.fr"),
+            new Client(2, "Robert", 5000, "robert@mail.fr"),
+            new Client(3, "Anthony", 20000, "anthony@mail.fr"),
+            new Client(4, "Michel", 100500, "michel@mail.fr"),
+            new Client(5, "Jean", 15000, "jean@mail.fr"),
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
